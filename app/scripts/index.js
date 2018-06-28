12ddb1a1f3886ba1eb423ad231d17a161050f6b6
bensek <bensek73@gmail.com>
2018-06-28 15:54:48 +0300

    First Working Conversion With API

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -17,24 +17,25 @@ function loadCurrencies(){
 	xHttp.send();
 }
 
-// function convertCurrency(){
-// 	var from=document.getElementById('from').value;
-// 	var to=document.getElementById('to').value;
-// 	var amount=document.getElementById('amount').value;
-// 	var result=document.getElementById('result');
-// 	if(from.length>0 && to.length>0 && amount.length>0){
-// 		var xHttp = new XMLHttpRequest();
-// 		xHttp.onreadystatechange=function(){
-// 			if(xHttp.readyState==4 && xHttp.status==200){
-// 				var obj = JSON.parse(this.responseText);
-// 				var fact=obj.value;
-// 				if(fact!=undefined){
-// 					result.innerHTML=parseFloat(amount)*parseFloat(fact);
-// 				}
-// 			}
-// 		}
-		
-// 	}
-// 	xHttp.open('GET', 'https://free.currencyconverterapi.com/api/v5/convert?q='+from+'_'+to'&compact=ultra', true);
-// 	xHttp.send();
-// }
+function convertCurrency(){
+	var from=document.getElementById('from').value;
+	var to=document.getElementById('to').value;
+	var amount=document.getElementById('amount').value;
+	var result=document.getElementById('result');
+	var query = from+'_'+to;
+	if(from.length>0 && to.length>0 && amount.length>0){
+		var xHttp = new XMLHttpRequest();
+		xHttp.onreadystatechange=function(){
+			if(xHttp.readyState==4 && xHttp.status==200){
+				var obj = JSON.parse(this.responseText);
+				var fact=obj[query];
+				console.log(fact);
+				if(fact!=undefined){
+					result.innerHTML=parseFloat(amount)*fact;
+				}
+			}
+		}
+		xHttp.open('GET', 'https://free.currencyconverterapi.com/api/v5/convert?q='+query+'&compact=ultra', true);
+		xHttp.send();
+	}
+}
